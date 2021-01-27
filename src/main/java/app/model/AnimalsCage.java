58bac13a48189fe9010fa2f620f15701b6d748a3
Usman Umarov <usman@umarov.ru>
2021-01-27 20:50:56 +0300

    2.1.3 v1

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,39 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
     private Animal animal;
+    private Timer timer;
+
+    @Autowired
+    @Qualifier("dog")
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    @Autowired
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
